eabb0dccdb55a3d04ca0e4dc18c518e02e49754c
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-02-11 15:46:43 +0300

    Added express and nodemon modules for server-side development

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const fn = function () {
-  console.log('Ben is coming');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: "Photo 1",
+    src: "Photo source",
+    desc: "Photo description"
+  }
+
+  res.status(200).send(photo);
+
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`The server has been started at port ${port}`);
+});
