919605c42b5d1acb0a3e1fbba7abb7c69903407e
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-02-11 21:27:02 +0300

    Added static HTML file, CSS stylesheets, project assets and other template  things(bootstrap, js, fontawsome...).

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: "Photo 1",
-    src: "Photo source",
-    desc: "Photo description"
-  }
+// MIDDLEWARES
+app.use(express.static('public'));
 
-  res.status(200).send(photo);
-
+app.get(['/', '/index.html'], (req, res) => {
+  res.sendFile(path.resolve('temp', 'index.html'));
 });
+
 
 const port = 3000;
 app.listen(port, () => {
